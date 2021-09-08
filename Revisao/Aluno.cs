31fef5b51393acde4ffb5156675c8573c28b0313
andrelula78 <andrelula78@yahoo.com>
2021-09-08 20:14:25 -0300

    Incluindo Aplicação Nota Aluno e Media Geral de Notas

---
+++ Revisao/Aluno.cs
@@ -0,0 +1,9 @@
+namespace Revisao
+{
+    public struct Aluno // usei o struct pois sera uma aplicação simples
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
